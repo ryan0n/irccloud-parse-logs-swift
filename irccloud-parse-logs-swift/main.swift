2a175bdd9856f0a4c5c72c32cab16f5ba1be2153
Ryan Wright <rywright@gmail.com>
2018-03-28 22:49:32 -0700

    Initial Commit

---
+++ irccloud-parse-logs-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  irccloud-parse-logs-swift
+//
+//  Created by Ryan Wright on 3/28/18.
+//  Copyright © 2018 Ryan Wright. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
